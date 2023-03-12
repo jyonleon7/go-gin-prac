20028c8c0b4067d253052851d4dc6510fa8cfce5
jyonleon7 <projecthapilabo@gmail.com>
2023-03-12 13:42:28 +0900

    環境変数をviper で管理

---
+++ utils/config.go
@@ -0,0 +1,29 @@
+package utils
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	// ディレクトリパス
+	viper.AddConfigPath(path)
+	// 拡張子の前
+	viper.SetConfigName("app")
+	// 拡張子
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
